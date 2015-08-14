eb47d8be531830077f3974a916c65cac3e7b2548
Michal Fiedorowicz <michal@qi-interactive.com>
2015-08-14 13:55:55 +0100

    MATA-81 (redy for styling)

--- views/social/view.php
+++ views/social/view.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\helpers\Inflector;
 
 /* @var $this yii\web\View */
 /* @var $model mata\contentblock\models\ContentBlock */
@@ -13,15 +14,29 @@ $this->params['breadcrumbs'][] = $this->title;
 
 <div class="module-entry-detail-view">
 
-<div><?= Html::a("Back to list view", sprintf("/mata-cms/%s/%s/list?SocialNetwork=%s", $this->context->module->id, $this->context->id, $model->SocialNetwork), ['id' => 'back-to-list-view']);?></div>
-
 <div class="content-block-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <?= DetailView::widget([
         'model' => $model,
+        'template' => '<div class="item row"><div class="two columns">{label}</div><div class="eight columns">{value}</div></div>',
+        'options' => [
+            'tag' => 'div',
+            'class' => 'details-view'
+        ]
     ]) ?>
 
 </div>
 </div>
+
+<script>
+
+    parent.mata.simpleTheme.header
+    .setBackToListViewURL("<?= sprintf("/mata-cms/%s/%s/list?SocialNetwork=%s", $this->context->module->id, $this->context->id, $model->SocialNetwork) ?>")
+    .setText('PREVIEW <?= Inflector::camel2words($model->SocialNetwork) ?> ENTRY: <?= $model->PublicationDate ?>')
+    .showBackToListView()
+    .hideVersions()
+    .show();
+
+</script>
